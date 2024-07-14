0f77df99a8afd653927ea5108b2e4d56c328a96c
jzentenos <jzentenos@unjbg.edu.pe>
2024-07-14 02:02:01 -0500

    Registros 4: Se halló el atleta con mayor medallas de un país ingresado por el usuario

--- registros4.cpp
+++ registros4.cpp
@@ -19,17 +19,30 @@ int main(){
 		cout<<"Cantidad de medallas: "; cin>>atleta[i].medallas;
 	}
 	string p;
+	int may_m=-100;
+	int aux=-100;
 	cout<<"Ingrese un pais: "; 
 	cin.ignore(); getline(cin, p);
+	cout<<"\n\tAtletas de "<<p<<endl;
 	for(int i=0; i<n; i++){
 		if(p==atleta[i].pais){
-			cout<<"Atleta "<<i+1<<endl;
+			cout<<"\n\tAtleta "<<i+1<<endl;
 			cout<<"Nombre: "<<atleta[i].nombre<<endl;
 			cout<<"Pais: "<<atleta[i].pais<<endl;
 			cout<<"Disciplina: "<<atleta[i].disciplina<<endl;
 			cout<<"Medallas: "<<atleta[i].medallas<<endl;
+			if(atleta[i].medallas>may_m){
+				may_m=atleta[i].medallas;
+				aux=i;
+			}
 		}
-		
+	}
+	if(aux!=-100){
+		cout<<"\nEl atleta con mayor medallas del pais "<<p<<" es: "<<endl;
+		cout<<atleta[aux].nombre<<" con "<<atleta[aux].medallas<<" medallas"<<endl;
+	}
+	else{
+		cout<<"\nCero Atletas registrados del pais "<<p<<endl;
 	}
 	return 0;
 }
