176a67a9bce1fa9e2fa8032671d3bb37e358d7bf
jzentenos <jzentenos@unjbg.edu.pe>
2024-07-14 01:05:04 -0500

    Registros 3: Se agrego el bucle do-while, para que imprima los datos de las personas que cumplen años en un mes ingresado por el usuario.

--- registros3.cpp
+++ registros3.cpp
@@ -17,6 +17,27 @@ int main(){
 		cout<<"Dia de nacimiento: "; cin>>datos[i].dia;
 		cout<<"Año de nacimiento: "; cin>>datos[i].periodo;
 	}
-
+	int m;
+	int cont=0;
+	do{
+		cout<<"Ingrese el numero de un mes: "; cin>>m;
+		cout<<"\n\tPersonas que cumplen el mes numero "<<m<<endl;
+		for(int i=0; i<n; i++){
+			if(m==datos[i].mes){
+				cout<<"\nNombre: "<<datos[i].nombre<<endl;
+				cout<<"Dia de naciminto: "<<datos[i].dia<<endl;
+				cout<<"Mes de nacimiento: "<<datos[i].mes<<endl;
+				cout<<"Año de nacimiento: "<<datos[i].periodo<<endl;
+				cont=1;
+			}
+			else{
+				cont=0;
+			}
+		}	
+		if(cont==0){
+			cout<<"\nNo existe personas que cumplan años en el mes "<<m<<endl;
+		}
+	}while(m!=0);
+	cout<<"Ingrese el numero de un mes: "; cin>>m;
 	return 0;
 }
