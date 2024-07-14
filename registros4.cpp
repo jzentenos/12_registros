1224347b53ba1362e7d00c802f70d20762f6629e
jzentenos <jzentenos@unjbg.edu.pe>
2024-07-14 01:48:51 -0500

    Registros 4: Se añadió la lectura de un país, este se evalua co  el país de los atletas y si coincide se imprime todos sus datos.

--- registros4.cpp
+++ registros4.cpp
@@ -18,5 +18,18 @@ int main(){
 		cout<<"Disciplina: "; getline(cin, atleta[i].disciplina);
 		cout<<"Cantidad de medallas: "; cin>>atleta[i].medallas;
 	}
+	string p;
+	cout<<"Ingrese un pais: "; 
+	cin.ignore(); getline(cin, p);
+	for(int i=0; i<n; i++){
+		if(p==atleta[i].pais){
+			cout<<"Atleta "<<i+1<<endl;
+			cout<<"Nombre: "<<atleta[i].nombre<<endl;
+			cout<<"Pais: "<<atleta[i].pais<<endl;
+			cout<<"Disciplina: "<<atleta[i].disciplina<<endl;
+			cout<<"Medallas: "<<atleta[i].medallas<<endl;
+		}
+		
+	}
 	return 0;
 }
