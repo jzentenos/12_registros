1584b61ccd056dc4ebf0113d670d21290cefd0ed
jzentenos <jzentenos@unjbg.edu.pe>
2024-07-13 23:59:36 -0500

    Registros 2: Se hallo la cantidad de personas que tengan más de 50 años de edad.

--- registros2.cpp
+++ registros2.cpp
@@ -16,12 +16,23 @@ int main(){
 		cin.ignore(); getline(cin,persona[i].nombre);
 		cout<<"DNI: "; cin>>persona[i].DNI;
 		cout<<"Edad: "; cin>>persona[i].edad;
-	}	
+	}
+	int cont=0;
+	for(int i=0; i<n; i++){
+		if(persona[i].edad>50){
+			cont++;
+		}
+	}
+	cout<<"\n\tCantidad de personas mayores a 50 años"<<endl;
+	cout<<cont;
+
+	
 	for(int i=0; i<n; i++){
 		cout<<"\n\tPersona "<<i+1<<endl;
 		cout<<"\nNombres completos: "<<persona[i].nombre;
 		cout<<"\nDNI: "<<persona[i].DNI;
 		cout<<"\nEdad: "<<persona[i].edad;		
 	}
+	
 	return 0;
 }
